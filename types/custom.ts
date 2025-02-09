4f65a1deb9e9eff053f6e673782e3cf7c319e8ec
nguyenanh92 <50308802+nguyenanh92@users.noreply.github.com>
2025-02-09 20:44:41 +0700

    f

--- types/custom.ts
+++ types/custom.ts
@@ -2,3 +2,4 @@ import { Database } from "./supabase";
 
 
 export type Todo = Database["public"]["Tables"]["todos"]["Row"]
+export type Settings = Database["public"]["Tables"]["settings"]["Row"]
--- types/supabase.ts
+++ types/supabase.ts
@@ -7,8 +7,60 @@ export type Json =
   | Json[]
 
 export type Database = {
+  graphql_public: {
+    Tables: {
+      [_ in never]: never
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      graphql: {
+        Args: {
+          operationName?: string
+          query?: string
+          variables?: Json
+          extensions?: Json
+        }
+        Returns: Json
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
   public: {
     Tables: {
+      settings: {
+        Row: {
+          id: number
+          inserted_at: string
+          is_active: boolean | null
+          key: string
+          user_id: string
+          value: string | null
+        }
+        Insert: {
+          id?: number
+          inserted_at?: string
+          is_active?: boolean | null
+          key: string
+          user_id: string
+          value?: string | null
+        }
+        Update: {
+          id?: number
+          inserted_at?: string
+          is_active?: boolean | null
+          key?: string
+          user_id?: string
+          value?: string | null
+        }
+        Relationships: []
+      }
       todos: {
         Row: {
           id: number
@@ -31,15 +83,7 @@ export type Database = {
           task?: string | null
           user_id?: string
         }
-        Relationships: [
-          {
-            foreignKeyName: "todos_user_id_fkey"
-            columns: ["user_id"]
-            isOneToOne: false
-            referencedRelation: "users"
-            referencedColumns: ["id"]
-          },
-        ]
+        Relationships: []
       }
     }
     Views: {
@@ -137,4 +181,19 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
     : never
